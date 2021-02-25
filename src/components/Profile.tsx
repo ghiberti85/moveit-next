d85c6534bc57b13ec10b097c2bc29b34194f495e
ghiberti85 <ghiberti85@gmail.com>
2021-02-25 17:34:27 -0300

    Fourth Class

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,6 +4,8 @@ import { ChallengesContext } from "../contexts/ChallengeContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/ghiberti85.png" alt="Devz" />
@@ -11,7 +13,7 @@ export function Profile() {
         <strong>Fernando Ghiberti</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
